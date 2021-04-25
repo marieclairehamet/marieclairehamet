5ddc1ed0e59e46296eaf7a87f864dfca46c90cc8
Marie Claire Hamet <marieclairehamet@gmail.com>
2021-04-25 16:28:24 -0400

    working

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,7 @@
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('css')
-  return {
-    passthroughFileCopy: true
-  }
-}
-module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('js')
-  return {
-    passthroughFileCopy: true
-  }
-}
-module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('images')
   return {
     passthroughFileCopy: true
